525554fdc59e263447d041642b46d4662dfa8633
khanh7atvtbn-source <khanh7atvtbn@gmail.com>
2026-01-08 16:18:28 +0700

    Update page.tsx

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,3 +1,15 @@
+"use client";
+
+import { useState } from "react";
+import Header from "@/components/Header";
+import ProductCard from "@/components/ProductCard";
+
+type CartItem = {
+  name: string;
+  price: number;
+  quantity: number;
+};
+
 export default function ProductsPage() {
   const [cart, setCart] = useState<CartItem[]>([]);
   const [showToast, setShowToast] = useState(false);
@@ -42,7 +54,7 @@ export default function ProductsPage() {
       {/* Products */}
       <section className="product-list">
         <div className="product-container">
-          <ProductCard name="Khoai tây chiên" price={20000} image="/images/Khoai.jpg" onAdd={() => addToCart("Khoai tây chiên", 20000)} />
+          <ProductCard name="Khoai tây chiên" price={20000} image="/images/khoai.jpg" onAdd={() => addToCart("Khoai tây chiên", 20000)} />
           <ProductCard name="Gà rán" price={30000} image="/images/garan.jpg" onAdd={() => addToCart("Gà rán", 30000)} />
           <ProductCard name="Bánh tráng trộn" price={15000} image="/images/banhtrangtron.jpg" onAdd={() => addToCart("Bánh tráng trộn", 15000)} />
           <ProductCard name="Nem chua rán" price={22000} image="/images/nemchuaran.jpg" onAdd={() => addToCart("Nem chua rán", 22000)} />
