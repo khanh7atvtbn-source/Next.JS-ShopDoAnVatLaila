37dedde6a48a510fed5cc72ffed9a56ca3fd16aa
khanh7atvtbn-source <khanh7atvtbn@gmail.com>
2026-02-03 22:46:32 +0700

    Update page.tsx

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import "./globals.css";
 
 export default function ProductsPage() {
   const [cart, setCart] = useState<CartItem[]>([]);
