74edd77414ec7ee77a1ce9a68f2d241adbb40585
khanh7atvtbn-source <khanh7atvtbn@gmail.com>
2026-02-04 02:10:03 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,10 @@ export default function HomePage() {
       <section className="banner">
         <div className="banner-content">
           <img src="/images/psd.jpg" alt="Banner" />
+          <div className="banner-buttons">
+            <a href="/products" className="cta-button">
+              MUA NGAY
+            </a>
 
           <h1 className="title-border">🍟 Shop Đồ Ăn Vặt Laila</h1>
 
